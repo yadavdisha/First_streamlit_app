18f85b7cf590c5ec626fd3b5cba1f431fbb2df13
Disha <34658273+yadavdisha@users.noreply.github.com>
2024-02-19 14:53:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
-streamlit.title("Hellooooooo Disha")
+streamlit.title("My Mom's New Healthy Dinner")
+streamlit.header('🍞Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avacado Toast')
+   
