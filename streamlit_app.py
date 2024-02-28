26b5f6844e676748a55d9c58ce1823cd71447257
Disha <34658273+yadavdisha@users.noreply.github.com>
2024-02-28 17:30:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit
- import requests
+import requests
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header('🍞Breakfast Menu')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
